48c378f5948b7f6451af59b3b34c2f027725015c
Alvin FREY <alvin.frey@protonmail.ch>
2020-09-28 18:17:22 +0200

    Adding Dataset processor

---
+++ MN-DatasetProcessor/main.cpp
@@ -0,0 +1,93 @@
+
+
+#include <iostream>
+#include <iostream>
+#include <filesystem>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+namespace fs = std::filesystem;
+
+std::string uploaded_folder_path = "./raw_data/";
+std::string processed_folder_path = "./processed_data/";
+
+int main()
+{
+
+	std::cout << "Dataset generator : " << std::endl;
+
+	std::cout << "CMFD CONVERSION" << std::endl;
+
+	// CMFD LOOP
+
+
+	int cmfdFileCount = distance(fs::directory_iterator(uploaded_folder_path + "/CMFD/"), fs::directory_iterator());
+	int cmfdLoopCount = 1;
+	int cmfdSeparationPoint = cmfdFileCount / 2;
+
+	for (const auto & entry : fs::directory_iterator(uploaded_folder_path + "/CMFD/")) {
+
+		cout << "Converting " << entry.path().filename() << " file" << endl;
+
+		string inputImagePath = samples::findFile(entry.path().string());
+		Mat inputImage = imread(inputImagePath, IMREAD_COLOR);
+
+		Mat outputImage;
+
+		resize(inputImage, outputImage, Size(), 0.1, 0.1, INTER_LINEAR);
+		cvtColor(outputImage, outputImage, COLOR_RGB2GRAY);
+
+		string outputImagePath;
+
+		if (cmfdLoopCount > cmfdSeparationPoint) {
+			outputImagePath = processed_folder_path + "/CMFD/training/" + entry.path().filename().string();
+		}
+		else {
+			outputImagePath = processed_folder_path + "/CMFD/testing/" + entry.path().filename().string();
+		}
+
+		imwrite(outputImagePath, outputImage);
+
+		cmfdLoopCount++;
+
+	}
+
+	cout << "IMFD CONVERSION" << endl;
+
+	// IMFD LOOP
+
+	int imfdFileCount = std::distance(fs::directory_iterator(uploaded_folder_path + "/IMFD/"), fs::directory_iterator());
+	int imfdLoopCount = 1;
+	int imfdSeparationPoint = imfdFileCount / 2;
+
+	for (const auto & entry : fs::directory_iterator(uploaded_folder_path + "/IMFD/")) {
+
+		std::cout << "Converting " << entry.path().filename() << " file" << std::endl;
+
+		std::string inputImagePath = cv::samples::findFile(entry.path().string());
+		cv::Mat inputImage = imread(inputImagePath, cv::IMREAD_COLOR);
+
+		cv::Mat outputImage;
+
+		cv::resize(inputImage, outputImage, cv::Size(), 0.1, 0.1, cv::INTER_LINEAR);
+		cv::cvtColor(outputImage, outputImage, cv::COLOR_RGB2GRAY);
+
+		std::string outputImagePath;
+
+		if (imfdLoopCount > imfdSeparationPoint) {
+			outputImagePath = processed_folder_path + "/IMFD/training/" + entry.path().filename().string();
+		}
+		else {
+			outputImagePath = processed_folder_path + "/IMFD/testing/" + entry.path().filename().string();
+		}
+
+		imwrite(outputImagePath, outputImage);
+
+		imfdLoopCount++;
+
+	}
+
+	return 0;
+
+}
